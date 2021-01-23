a9f96563aa40dd59ccbb1472525400f25d0e4a56
Mason A. Howe <mason.a.howe@gmail.com>
2021-01-23 12:15:53 -0700

    add prompt

--- Index.js
+++ Index.js
@@ -1,5 +1,25 @@
 const Employee = require("./lib/Employee");
 const fs = require("fs");
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
+const questions = require("./lib/questions");
 
 
+
+
+prompts();
+
+function prompts() {
+
+    let data;
+  inquirer.prompt(questions.employeeQuestions).then((anwsers) => {
+      console.log(anwsers.employeeType);
+    if(anwsers.employeeType == "Manager"){
+        inquirer.prompt(questions.managerQuestions)
+    }else if(anwsers.employeeType == "Engineer"){
+        inquirer.prompt(questions.engineerQuestions);
+    }else if(anwsers.employeeType == "Intern"){
+        inquirer.prompt(questions.internQuestions);
+    }else{};
+    
+  });
+}
