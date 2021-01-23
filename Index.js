ce775c74a2d38a97da317b76ed132ef708ff0e5f
Mason Howe <mason.a.howe@gmail.com>
2021-01-23 15:00:39 -0700

    add get info function

--- Index.js
+++ Index.js
@@ -1,25 +1,6 @@
 const Employee = require("./lib/Employee");
 const fs = require("fs");
 const inquirer = require("inquirer");
-const questions = require("./lib/questions");
+const getInfo = require("./lib/questions");
 
-
-
-
-prompts();
-
-function prompts() {
-
-    let data;
-  inquirer.prompt(questions.employeeQuestions).then((anwsers) => {
-      console.log(anwsers.employeeType);
-    if(anwsers.employeeType == "Manager"){
-        inquirer.prompt(questions.managerQuestions)
-    }else if(anwsers.employeeType == "Engineer"){
-        inquirer.prompt(questions.engineerQuestions);
-    }else if(anwsers.employeeType == "Intern"){
-        inquirer.prompt(questions.internQuestions);
-    }else{};
-    
-  });
-}
+getInfo();
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,9 +1,12 @@
 const importClasses = require("../lib/Employee");
 
+test("Test Intern Class", () => {
+  const Intern = new importClasses.Intern(
+    "mason",
+    20,
+    "masonhowe@msn.com",
+    "University of Utah"
+  );
 
-test("Test Intern Class", () =>{
-    const Intern = new importClasses.Intern("mason",20,"masonhowe@msn.com", "University of Utah");
-
-    expect(Intern.getRole()).toBe()
-    
+  expect(Intern.getRole()).toBe("Intern");
 });
